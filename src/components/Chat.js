b4aff19e02c0f3f19c138d8bbe9faaa07187faeb
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-05 20:13:54 +0200

    feat: initial commit for src/components/Chat.js

---
+++ src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chat = () => {
+    return (
+        <div>
+            <h2>Trò chuyện với đồng nghiệp</h2>
+            {/* Thêm chức năng chat ở đây */}
+        </div>
+    );
+};
+
+export default Chat;

ca4a058129ce75e61f5de592497f565ade01d5ae
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-05 20:14:10 +0200

    Fix greeting message for clarity.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Remote Work Helper!');
+  res.send('Welcome to Remote Work Helper!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Máy chủ đang chạy trên http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });

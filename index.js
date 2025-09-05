7806a71251bb1caddb0b323ccc0e2e26d4490f3b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-05 20:17:37 +0200

    Format code and add a middleware for JSON body parsing

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Remote Work Helper!');
 });
